d5bbcc1d183b273deea8b51f9cfe757344b45797
zhaojunhui <gamedirty@hotmail.com>
2016-10-11 18:25:03 +0800

    d

---
+++ app/src/main/java/com/elianshang/yougong/sales/receiver/ConnectionReceiver.java
@@ -0,0 +1,17 @@
+package com.elianshang.yougong.sales.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author zjh
+ * @description
+ * @date 16/10/11.
+ */
+
+public class ConnectionReceiver extends BroadcastReceiver {
+  @Override public void onReceive(Context context, Intent intent) {
+
+  }
+}
